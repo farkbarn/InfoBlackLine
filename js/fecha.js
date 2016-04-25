3a4ed11eeb793f5d9f74e65cea66ace1017795da
Frank Barrera <frankb2320@gmail.com>
2016-04-25 14:56:15 -0430

    agregando reloj

---
+++ js/fecha.js
@@ -0,0 +1,12 @@
+var mydate=new Date()
+var year=mydate.getYear()
+if (year < 1000)
+year+=1900
+var day=mydate.getDay()
+var month=mydate.getMonth()
+var daym=mydate.getDate()
+if (daym<10)
+	daym="0"+daym 
+	var dayarray=new Array("Domingo,","Lunes,","Martes,","Miércoles,","Jueves,","Viernes,","Sábado,")
+	var montharray=new Array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre")
+	document.write("<p class='fecha colorfont2'>"+'Barquisimeto, '+"<span class='de'>"+" "+daym+" de "+"</span>"+montharray[month]+" del "+year+''+"</p>");
