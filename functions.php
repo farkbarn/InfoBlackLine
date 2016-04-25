ff7ad7b457363c28d904c60aac7253c15e689ad2
Frank Barrera <frankb2320@gmail.com>
2016-04-25 15:30:16 -0430

    agregando parametros y personalización

--- functions.php
+++ functions.php
@@ -1,5 +1,34 @@
 <?php
 
+
+
+
+
+//Contador de caracteres en extracto
+function excerpt_count_js(){
+      echo '<script>jQuery(document).ready(function(){
+jQuery("#postexcerpt .handlediv").after("<div style=\"position:absolute;top:0px;right:30px;color:#666;\"><small></small><input type=\"text\" value=\"0\" maxlength=\"3\" size=\"3\" id=\"excerpt_counter\" readonly=\"\" style=\"background:#fff;\"> <small>caracter(es).</small></div>");
+     jQuery("#excerpt_counter").val(jQuery("#excerpt").val().length);
+     jQuery("#excerpt").keyup( function() {
+     jQuery("#excerpt_counter").val(jQuery("#excerpt").val().length);
+   });
+});</script>';
+}
+add_action( 'admin_head-post.php', 'excerpt_count_js');
+add_action( 'admin_head-post-new.php', 'excerpt_count_js');
+
+//Contador de caracteres en titulo
+function titulo_contador_js(){
+      echo '<script>jQuery(document).ready(function(){
+jQuery("#titlewrap").after("<div style=\"position:absolute;top:-32px;right:0px;color:#666;\"><small></small><input type=\"text\" value=\"0\" maxlength=\"3\" size=\"3\" id=\"tit_counter\" readonly=\"\" style=\"background:#fff;\"> <small>Máximo 90 caracter(es).</small></div>");
+	jQuery("#tit_counter").val(jQuery("#title").val().length);
+	jQuery("#title").keyup( function() {
+	jQuery("#tit_counter").val(jQuery("#title").val().length);
+    });
+});</script>';}
+add_action('admin_head-post.php','titulo_contador_js');
+add_action('admin_head-post-new.php','titulo_contador_js');
+
 // PERSONALIZAR EL ADMIN LOGIN
 function style_adm() { ?>
     <style type="text/css">
@@ -17,12 +46,14 @@ function style_adm() { ?>
     </style>
 <?php }
 add_action( 'login_enqueue_scripts', 'style_adm' );
+
 /*ELIMINAR NOTIFICACION DE ACTUALIZACION PARA NO ADM*/
 global $user_login;
 get_currentuserinfo();
 if ($user_login !== "admin") {
 add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
 add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );}
+
 // UPDATE TEMA DESDE WP
 function github_check_update( $transient ) {
 $usergithub=farkbarn;
@@ -49,9 +80,7 @@ $ramastable=master;
 }
 add_filter( 'pre_set_site_transient_update_themes', 'github_check_update' );
 //Cambiar el pie de pagina del panel de Administración
-function change_footer_admin() {
-    echo '&copy;2016 Copyright EL INFORMADOR. Todos los derechos reservados - Web creada por <a href="http://www.elinformador.com.ve">El Informador</a>';  
-}
+function change_footer_admin() {echo '&copy;2016 Copyright EL INFORMADOR. Todos los derechos reservados - Web creada por <a href="http://www.elinformador.com.ve">El Informador</a>';}
 add_filter('admin_footer_text', 'change_footer_admin');
 //ELIMINAR EJECUCION DE CODIGO HTML EN COMMENTS
 add_filter('pre_comment_content', 'wp_specialchars');
