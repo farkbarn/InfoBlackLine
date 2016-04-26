7f9160d3cd3320b113a119db4defef9843876675
Frank Barrera <frankb2320@gmail.com>
2016-04-26 16:28:22 -0430

    retirando errores

--- index.php
+++ index.php
@@ -1315,32 +1315,4 @@
 					</section>
 				</section>
 			</section>
-			
-			<footer>
-				<section class='pie'>
-					<a href="#"><p class='infopie colorfont1' id='Informador'>El Informador<span>.com.ve</span></p></a>
-					<p class='colorfont1 txtcentro'>rem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temsectetur adipiscinsectetur adipiscing elit, sed do eiusmod temsectetur adipiscing elitg elit</p>
-					<section class='redpie'>
-						<ul class=''>
-							<li class='tg' id='tg'><a href='https://telegram.me/elinformadorve' alt='Telegram'><span class="icon-telegram"></span></a></li>
-							<li class='tw' id='tw'><a href='https://twitter.com/elinformadorve' alt='Twitter'><span class='icon-twitter3'></span></a></li>
-							<li class='fb' id='fb'><a href='https://www.facebook.com/elinformadorve/' alt='Facebook'><span class='icon-facebook3'></span></a></li>
-							<li class='yt' id='yt'><a href='https://www.youtube.com/channel/UCPb4vjcbn4j0BQ_RQ-72X6Q/' alt='YouTube'><span class='icon-youtube4'></span></a></li>
-							<li class='gp' id='gp'><a href='https://plus.google.com/b/107602064440905060759/+ElinformadorVe/' alt='Google+'><span class='icon-google-plus3'></span></a></li>
-							<li class='in' id='in'><a href='https://instagram.com/elinformadorve' alt='Instagram'><span class='icon-instagram'></span></a></li>
-							<li class='rs' id='rs'><a href='http://www.elinformador.com.ve/feed' alt='RSS'><span class='icon-feed4'></span></a></li>
-						</ul>
-					</section>
-				</section>
-			</footer>
-		</section>
-	
-    <section class='ads'>
-		
-	</section>
-    <section class='ads'>
-		
-	</section>
-    
-	</body>
-</html>
+<?php get_footer();?>
