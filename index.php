85bf6385d8c86a7b690b29e01bb01a0e96ccd85a
Frank Barrera <frankb2320@gmail.com>
2016-04-26 15:58:52 -0430

    agregando menu

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+$dirtem=get_template_directory_uri();.'/';
+$dircat=get_site_url();.'/temas/'
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 	<head>
@@ -5,13 +10,13 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="http://fonts.googleapis.com/css?family=Roboto:700,400" rel="stylesheet" type="text/css" media="screen">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-		<link href="<?php echo get_template_directory_uri();?>/css/reset.css" rel="stylesheet" type="text/css" media="screen">
-		<link href="<?php echo get_template_directory_uri();?>/style.css" rel="stylesheet" type="text/css" media="screen">
-		<link href="<?php echo get_template_directory_uri();?>/responsive.css" rel="stylesheet" type="text/css" media="screen">
-		<link href="<?php echo get_template_directory_uri();?>/fonts.css" rel="stylesheet" type="text/css" media="screen">
-		<link href="<?php echo get_template_directory_uri();?>/css/bjqs.css" rel="Stylesheet" type="text/css">
-		<script src="<?php echo get_template_directory_uri();?>/js/bjqs.min.js"></script>
-		<script src="<?php echo get_template_directory_uri();?>/js/delay.js"></script>
+		<link href="<?php echo $dirtem;?>css/reset.css" rel="stylesheet" type="text/css" media="screen">
+		<link href="<?php echo $dirtem;?>style.css" rel="stylesheet" type="text/css" media="screen">
+		<link href="<?php echo $dirtem;?>responsive.css" rel="stylesheet" type="text/css" media="screen">
+		<link href="<?php echo $dirtem;?>fonts.css" rel="stylesheet" type="text/css" media="screen">
+		<link href="<?php echo $dirtem;?>css/bjqs.css" rel="Stylesheet" type="text/css">
+		<script src="<?php echo $dirtem;?>js/bjqs.min.js"></script>
+		<script src="<?php echo $dirtem;?>js/delay.js"></script>
 		<script async type="text/javascript">
 		jQuery(document).ready(function($){
 			$('#Slider').bjqs({'prevtext':'<span class="icon-circle-left"></span>','nexttext':'<span class="icon-circle-right"></span>','keyboardnav':true,'animtype':'fade','height':480,'width':710,'responsive':true,'showcontrols':true,'showmarkers':false,'randomstart':true,'animspeed':10000});
@@ -157,26 +162,26 @@
 							<h1><a href="#" class='infotit colorfont4' id='Informador' >El Informador<span>.com.ve</span></a></h1>
 						</section>
 						<section>
-							<script src='<?php echo get_template_directory_uri();?>/js/fecha.js'></script>
+							<script src='<?php echo $dirtem;?>js/fecha.js'></script>
 						</section>
 					</section>
 					<section>
-						<object class="logo" type="image/svg+xml" data="<?php echo get_template_directory_uri();?>/img/logo.svg">
-							<img class="logo" src="<?php echo get_template_directory_uri();?>/img/logo.png" alt="El Informador" title="El Informador">
+						<object class="logo" type="image/svg+xml" data="<?php echo $dirtem;?>img/logo.svg">
+							<img class="logo" src="<?php echo $dirtem;?>img/logo.png" alt="El Informador" title="El Informador">
 						</object>
 					</section>
 					<section class='menu color2'>
 						<nav>
 							<ul class='menugrupo'>
-								<li id="item-ini"><a class="" href="#">Inicio</a><div class="item-pie-ini bgini"></div></li>
-								<li id="item-lar"><a class="" href="#">Lara</a><div class="item-pie-lar bglar"></li>
-								<li id="item-nac"><a class="" href="#">Nacionales</a><div class="item-pie-ini bgnac"></li>
-								<li id="item-int"><a class="" href="#">Internacionales</a><div class="item-pie-ini bgint"></li>
-								<li id="item-dep"><a class="" href="#">Deportes</a><div class="item-pie-ini bgdep"></li>
-								<li id="item-suc"><a class="" href="#">Sucesos</a><div class="item-pie-ini bgsuc"></li>
-								<li id="item-act"><a class="" href="#">Actualidad</a><div class="item-pie-ini bgact"></li>
-								<li id="item-eco"><a class="" href="#">Econom&iacute;a</a><div class="item-pie-ini bgeco"></li>
-								<li id="item-tec"><a class="" href="#">Salud y Belleza</a><div class="item-pie-ini bgtec">
+								<li id="item-ini"><a class="" href="<?php echo $dircat;?>">Inicio</a><div class="item-pie-ini bgini"></div></li>
+								<li id="item-lar"><a class="" href="<?php echo $dircat;?>lara">Lara</a><div class="item-pie-lar bglar"></li>
+								<li id="item-nac"><a class="" href="<?php echo $dircat;?>nacionales">Nacionales</a><div class="item-pie-ini bgnac"></li>
+								<li id="item-int"><a class="" href="<?php echo $dircat;?>internacionales">Internacionales</a><div class="item-pie-ini bgint"></li>
+								<li id="item-dep"><a class="" href="<?php echo $dircat;?>deportes">Deportes</a><div class="item-pie-ini bgdep"></li>
+								<li id="item-suc"><a class="" href="<?php echo $dircat;?>sucesos">Sucesos</a><div class="item-pie-ini bgsuc"></li>
+								<li id="item-act"><a class="" href="<?php echo $dircat;?>actualidad">Actualidad</a><div class="item-pie-ini bgact"></li>
+								<li id="item-eco"><a class="" href="<?php echo $dircat;?>economia">Econom&iacute;a</a><div class="item-pie-ini bgeco"></li>
+								<li id="item-tec"><a class="" href="<?php echo $dircat;?>salud_y_belleza">Salud y Belleza</a><div class="item-pie-ini bgtec">
 							</ul>
 						</nav>
 					</section>
