c7d2cbc518bb76649e934b4d8bf09338b9c3d31a
Frank Barrera <frankb2320@gmail.com>
2016-04-27 10:09:30 -0430

    separando ads cabezal

--- header.php
+++ header.php
@@ -44,23 +44,23 @@ $dircat=get_site_url().'/temas/';
 				<ul class='bjqs'>
 					<li>
 						<aside class='A1 mxwiads1'>
-						    <?php include('ads/A1.js');?>
+						    <?php //include('ads/A1.js');?>
 						</aside>
 					</li>
 					<li>
 						<aside class='A2 mxwiads2'>
-						    <?php include('ads/A2.js');?>
+						    <?php //include('ads/A2.js');?>
 						</aside>
 						<aside class='A3 mxwiads3'>
-						    <?php include('ads/A3.js');?>
+						    <?php //include('ads/A3.js');?>
 						</aside>
 					</li>
 					<li>
 						<aside class='B1 mxwiads4'>
-						    <?php include('ads/B1.js');?>
+						    <?php //include('ads/B1.js');?>
 						</aside>
 						<aside class='B2 mxwiads5'>
-						    <?php include('ads/B2.js');?>
+						    <?php //include('ads/B2.js');?>
 						</aside>
 					</li>
 				</ul>
