aad399acbde5a470a78bed75da4acdcdd6d1c3b9
Frank Barrera <frankb2320@gmail.com>
2016-04-28 10:12:24 -0430

    corrección ads

---
+++ ads4.php
@@ -0,0 +1,17 @@
+						    <aside class='ads4 mxwiads' id='ads4'>
+							<ul class='bjqs'>
+							    <li>
+								<aside class='E1 mxwiads'>
+								    <?php //include('ads/E1.js');?>
+								</aside>
+							    </li>
+							    <li>
+								<aside class='E2 mxwiads'>
+								    <?php //include('ads/E2.js');?>
+								</aside>
+								<aside class='E3 mxwiads'>
+								    <?php //include('ads/E3.js');?>
+								</aside>
+							    </li>
+							</ul>
+						    </aside>
---
+++ ads5.php
@@ -0,0 +1,17 @@
+						    <aside class='ads5 mxwiads' id='ads5'>
+							<ul class='bjqs'>
+							    <li>
+								<aside class='F1 mxwiads'>
+								    <?php //include('ads/F1.js');?>
+								</aside>
+							    </li>
+							    <li>
+								<aside class='F2 mxwiads'>
+								    <?php //include('ads/F2.js');?>
+								</aside>
+								<aside class='F3 mxwiads'>
+								    <?php //include('ads/F3.js');?>
+								</aside>
+							    </li>
+							</ul>
+						    </aside>
