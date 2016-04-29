9d0b7dcce1721e5974ef4ceae5603b431e380041
Frank Barrera <frankb2320@gmail.com>
2016-04-29 14:11:49 -0430

    -- funciones

--- functions.php
+++ functions.php
@@ -1,17 +1,7 @@
 <?php
 include(TEMPLATEPATH.'/includes/images.php');
-
 the_post_thumbnail( $size, $attr );
-add_theme_support( 'post-thumbnails');
-add_image_size( 'img_col1', 450, 300, true );
-add_image_size( 'img_col2', 190, 230, false );
-add_image_size( 'img_sil2', 190, 150, true );
-add_image_size( 'img_slid1', 690, 500, true );
-add_image_size( 'img_slid2', 290, 140, true );
 
-?>
-
-<?php
 // AGREGANDO PARAMETROS DE IMAGEN
 //the_post_thumbnail($size,$attr);
 add_theme_support('post-thumbnails');
