821adc8a0390c4e96b4f0f86efa2c03e692b4e59
Frank Barrera <frankb2320@gmail.com>
2016-04-29 08:19:31 -0430

    organizando cod, creando single

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<?php $tnot=0;$ncol2=0;$dirtem=get_template_directory_uri().'/'; $dircat=get_site_url().'/temas/';?>
+<?php $tnot=0;$ncol2=0;$dirtem=get_template_directory_uri().'/'; $dircat=get_site_url().'/temas/';$idcat='-15,-1';$tnot=19;$nsli=5;$npsli=2;$nads=5;$arridpost=array();?>
 <!DOCTYPE html>
 <html lang="es">
 	<head>
