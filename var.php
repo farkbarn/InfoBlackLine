da34354682fb833dc94590ea881796b9ebdd02bf
Frank Barrera <frankb2320@gmail.com>
2016-05-02 15:14:53 -0430

    agregando filtro

--- var.php
+++ var.php
@@ -1,7 +1,7 @@
 <?php
 $tnot=0;$ncol2=0;$dirtem=get_template_directory_uri().'/'; $dircat=get_site_url().'/temas/';
 /*$idcat='-15,-1,-9';*/$tnot=19;$nsli=5;$npsli=2;$nads=5;$arridpost=array();
-$no_idcathome=array(-1,-9);
-$no_idcatcol2=array();
+$no_idcathome=array(-1,-9,-12);
+$no_idcatcol2=array(-12);
 ?>
 
