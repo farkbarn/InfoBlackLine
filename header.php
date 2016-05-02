c62291c535d6932a4bcda49a7c7c7812ef23414d
Frank Barrera <frankb2320@gmail.com>
2016-05-02 09:11:21 -0430

    mod para menufix

--- header.php
+++ header.php
@@ -40,12 +40,14 @@ $(window).scroll(function(){
 	$('#logo').addClass('logofix');
 	$('#menu').addClass('menufix');
 	$('ul.menugrupo').addClass('menugrupofix');
+	$('.centrador').addClass('centradorfix');
 	} else {
 	$('#cabezal').removeClass('cabezalfix');
 	$('#infotit').removeClass('infotitfix');
 	$('#logo').removeClass('logofix');
 	$('#menu').removeClass('menufix');
 	$('ul.menugrupo').removeClass('menugrupofix');
+	$('.centrador').removeClass('centradorfix');
 	}
 });
 		</script>
