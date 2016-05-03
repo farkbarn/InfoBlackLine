dd6b9601b1252e1182f6149388074f9d4ec48d3d
Frank Barrera <frankb2320@gmail.com>
2016-05-03 13:42:58 -0430

    columnistas

--- columnistas.php
+++ columnistas.php
@@ -1,48 +1,35 @@
-<!-- INICIO COMUNISTA -->
 						<section class='tagsec'>
 						    <section class='recnar'></section>
 						    <section class='recneg'></section>
 						    <p class="txteti">Columnistas</p>
 						</section>
-<!-- FIN COMUNISTA -->
-<!-- INICIO COLUMNISTA -->
 						<article class='colum'>
 						    <figure>
 							<a href='<?php echo $dircat;?>col_juan'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 1'></a>
 							<figcaption>Alan Brito</figcaption>
 						    </figure>
 						</article>
-<!-- FIN COLUMNISTA -->
-<!-- INICIO COLUMNISTA -->
 						<article class='colum'>
 						    <figure>
 							<a href='<?php echo $dircat;?>col_pedro'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 2'></a>
 							<figcaption>Rosa Melo</figcaption>
 						    </figure>
 						</article>
-<!-- FIN COLUMNISTA -->
-<!-- INICIO COLUMNISTA -->
 						<article class='colum'>
 						    <figure>
-							<a href='<?php echo $dircat;?>col_pedro'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 3'></a>
+							<a href='<?php echo $dircat;?>col_marcos'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 3'></a>
 							<figcaption>Elmer Kado</figcaption>
 						    </figure>
 						</article>
-<!-- FIN COLUMNISTA -->
-<!-- INICIO COLUMNISTA -->
 						<article class='colum'>
 						    <figure>
-							<a href='<?php echo $dircat;?>col_pedro'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 4'></a>
+							<a href='<?php echo $dircat;?>col_manuel'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 4'></a>
 							<figcaption>Marc Tillo</figcaption>
 						    </figure>
 						</article>
-<!-- FIN COLUMNISTA -->
-<!-- INICIO COLUMNISTA -->
 						<article class='colum'>
 						    <figure>
-							<a href='<?php echo $dircat;?>col_pedro'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 5'></a>
+							<a href='<?php echo $dircat;?>colum_rafael'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 5'></a>
 							<figcaption>Elvis Teck	</figcaption>
 						    </figure>
 						</article>
-<!-- FIN COLUMNISTA -->
-
