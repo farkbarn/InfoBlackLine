08bd4b1b73983c4b57140ae966d046395291f47f
Frank Barrera <frankb2320@gmail.com>
2016-05-03 12:32:33 -0430

    add mas leidas con plugin top10

--- masleido.php
+++ masleido.php
@@ -1,23 +1,10 @@
 						<section class='block3'>
 						    <?php include('ads8.php');?>
 						    <?php include('ads9.php');?>
-<!-- INICIO TAG LEIDO -->
 						    <section class='tagsec'>
 							<section class='recnar'></section>
 							<section class='recneg'></section>
 							<p class="txteti">Lo M&aacute;s Le&iacute;do</p>
 						    </section>
-<!-- FIN TAG LEIDO -->
 						    <?php if (!dynamic_sidebar('Mas Leidos'));?>
-<!-- INICIO MAS LEIDO -->
-							<article class='maslei'>
-							    <figure>
-								    <a href='#'><img class='img' src='http://ximg.es/110x60/525252/fff&text=Foto++-++110+x+60'></a>
-							    </figure>
-							    <header>
-								    <a href='#'><h4>T&iacute;tulo de Nota</h4></a>
-							    </header>
-							    <p>adipiscing elit. Duis eget metus tincidunt, porta massa vitae,</p>
-							</article>
-<!-- FIN MAS LEIDO -->
 						</section>
