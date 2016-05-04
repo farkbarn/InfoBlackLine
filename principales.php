79f32ef14583e57439a1ec8f47784df1865a7471
Frank Barrera <frankb2320@gmail.com>
2016-05-04 14:10:19 -0400

    agregando red Gp

--- principales.php
+++ principales.php
@@ -99,7 +99,6 @@
 									<li class='tw' id='tw'><a alt='Twitter' href="javascript:void(0);" onclick='<?php echo 'window.open("https://twitter.com/intent/tweet?url='; echo bitly().'&text='.get_the_title().'&related=&via=elinformadorve&original_referer='.get_permalink(); ?>","ventanacompartir", "toolbar=0, status=0, width=650, height=450");'><span class='icon-twitter3'></span></a></li>
 									<li class='fb' id='fb'><a alt='Facebook' href="javascript:void(0);" onclick='<?php echo 'window.open("https://www.facebook.com/sharer/sharer.php?sdk=joey&u='.get_permalink().'&t='.get_the_title().'&display=popup&ref=web&src=button'; ?>","ventanacompartir", "toolbar=0, status=0, width=650, height=450");'><span class='icon-facebook3'></span></a></li>
 									<li class='pg' id='pg'><a alt='Google+' href="javascript:void(0);" onclick='<?php echo 'window.open("https://plus.google.com/share?url='.get_permalink().'&t='.get_the_title().'&gpsrc=gplp0&pli=1'; ?>","ventanacompartir", "toolbar=0, status=0, width=650, height=450");'><span class='icon-google-plus3'></span></a></li>
-									
 								    </ul>
 								</section>
 							</figcaption>
