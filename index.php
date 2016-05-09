0aa828c5bf0ed119569d1c8d2e8586a547144678
Frank Barrera <frankb2320@gmail.com>
2016-05-09 16:55:54 -0400

    obtener width para controlar carga de datos

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php get_header();?>
 <?php include('var.php');?>
 <?php
-$wid='<script type="text/javascript">document.write(screen.availWidth);</script>';
-
+$actsli='<script>
+	    if(screen.availWidth > 1000){document.write(1);}
+	    else{document.write(0);}
+	</script>';
+	echo $actsli;
+	echo gettype($actsli);
 ?>
 <script async type="text/javascript">jQuery(document).ready(function($){$('#Slider').bjqs({'prevtext':'<span class="icon-circle-left"></span>','nexttext':'<span class="icon-circle-right"></span>','keyboardnav':true,'animtype':'fade','height':515,'width':710,'responsive':true,'showcontrols':true,'showmarkers':false,'randomstart':true,'animspeed':10000});});</script>
 				<section class='superior'>
 				    <?php query_posts(array('posts_per_page'=>$tnot,'post__not_in'=>$arridpost,'category__not_in' => $no_idcathome)); ?>
-					<?php if (1 > 0){include('principales.php');}?>
+					<?php if ($actsli == "1"){include('principales.php');}?>
 					<?php if ($ads){include('ads2.php');}?>
 					<section class='notas'>
 						<section class='block1'>							
