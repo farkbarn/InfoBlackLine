772e533eef0a7e8c727e21ac76689002a1670a13
Frank Barrera <frankb2320@gmail.com>
2016-05-10 10:51:19 -0400

    reemplazando variable ads

--- category.php
+++ category.php
@@ -1,10 +1,10 @@
 <?php get_header();?>
     <?php include('var.php');$nads=5;$arridpost=array();$i=1;?>
     				<section class='superior'>
-					<?php if ($ads){include('ads2.php');}?>
+					<?php if ($_SESSION['ads']){include('ads2.php');}?>
 					<section class='nota'>
 						<section class='block1'>							
-						    <?php if ($ads){include('ads3.php');}?>
+						    <?php if ($_SESSION['ads']){include('ads3.php');}?>
 <!-- INICIO NOTA COL1 -->
 						    <?php while (have_posts()):the_post();?>
 						    <?php $arridpost[]=get_the_id();?>
@@ -54,8 +54,8 @@
 							<p class='txtnot'><?php echo the_excerpt_max(230);?></p>
 							<a class='rrssnot colorfont5' href='<?php echo get_permalink();?>'><div class="flecha flecol5"></div>ver art&iacute;culo completo</a>
 						    </article>
-						    <?php if ($i==($nads)){if ($ads){include('ads4.php');}}?>
-						    <?php if ($i==($nads*2)){if ($ads){include('ads5.php');}}?>
+						    <?php if ($i==($nads)){if ($_SESSION['ads']){include('ads4.php');}}?>
+						    <?php if ($i==($nads*2)){if ($_SESSION['ads']){include('ads5.php');}}?>
 						    <?php $i++; endwhile; wp_reset_query();?>
 						    <center> <?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?></center>
 <!-- FIN NOTA COL1 -->
