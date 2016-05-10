8e068a7043a575536bd046ae5c09ab068c8bb1ff
Frank Barrera <frankb2320@gmail.com>
2016-05-10 10:57:02 -0400

    retirando variables de var.php

--- var.php
+++ var.php
@@ -1,10 +1,13 @@
 <?php
-
 session_start();
 
-$ads=true;
-$tnot=0;$ncol2=0;$dirtem=get_template_directory_uri().'/'; $dircat=get_site_url().'/temas/';
-/*$idcat='-15,-1,-9';*/$tnot=20;$nsli=5;$npsli=2;$nads=5;$arridpost=array();
+$_SESSION['ads']=true;
+$_SESSION['tnot']=20;
+
+
+//$ads=true;
+/*$tnot=0;*/$ncol2=0;$dirtem=get_template_directory_uri().'/'; $dircat=get_site_url().'/temas/';
+/*$idcat='-15,-1,-9';$tnot=20;*/$nsli=5;$npsli=2;$nads=5;$arridpost=array();
 $no_idcathome=array(-1,-9,-12);
 $no_idcatcol2=array(-12,-9);
 $i=1;
