1f7c00bccb2cd3e1d4494a981bd501a15f525123
Frank Barrera <frankb2320@gmail.com>
2016-05-10 10:40:08 -0400

    moviendo session start

--- var.php
+++ var.php
@@ -1,4 +1,7 @@
 <?php
+
+session_start();
+
 $ads=true;
 $tnot=0;$ncol2=0;$dirtem=get_template_directory_uri().'/'; $dircat=get_site_url().'/temas/';
 /*$idcat='-15,-1,-9';*/$tnot=20;$nsli=5;$npsli=2;$nads=5;$arridpost=array();
