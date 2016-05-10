d58f3a061d6fe3ec6c993eead652f92705260872
Frank Barrera <frankb2320@gmail.com>
2016-05-10 11:30:57 -0400

    agregando dircat

--- columnistas.php
+++ columnistas.php
@@ -5,31 +5,31 @@
 						</section>
 						<article class='colum'>
 						    <figure>
-							<a href='<?php echo $dircat;?>col_juan'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 1'></a>
+							<a href='<?php echo $_SESSION['dircat'];?>col_juan'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 1'></a>
 							<figcaption>Alan Brito</figcaption>
 						    </figure>
 						</article>
 						<article class='colum'>
 						    <figure>
-							<a href='<?php echo $dircat;?>col_pedro'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 2'></a>
+							<a href='<?php echo $_SESSION['dircat'];?>col_pedro'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 2'></a>
 							<figcaption>Rosa Melo</figcaption>
 						    </figure>
 						</article>
 						<article class='colum'>
 						    <figure>
-							<a href='<?php echo $dircat;?>col_marcos'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 3'></a>
+							<a href='<?php echo $_SESSION['dircat'];?>col_marcos'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 3'></a>
 							<figcaption>Elmer Kado</figcaption>
 						    </figure>
 						</article>
 						<article class='colum'>
 						    <figure>
-							<a href='<?php echo $dircat;?>col_manuel'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 4'></a>
+							<a href='<?php echo $_SESSION['dircat'];?>col_manuel'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 4'></a>
 							<figcaption>Marc Tillo</figcaption>
 						    </figure>
 						</article>
 						<article class='colum'>
 						    <figure>
-							<a href='<?php echo $dircat;?>colum_rafael'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 5'></a>
+							<a href='<?php echo $_SESSION['dircat'];?>colum_rafael'><img class='img' src='http://ximg.es/260x100/525252/fff&text=Columnista 5'></a>
 							<figcaption>Elvis Teck	</figcaption>
 						    </figure>
 						</article>
