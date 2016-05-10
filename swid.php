de204d5dd794fd805a320d3c73acd9077b6ba6b1
Frank Barrera <frankb2320@gmail.com>
2016-05-10 10:24:44 -0400

    creando get para obtener width de pantalla

---
+++ swid.php
@@ -0,0 +1,10 @@
+<?php session_start();
+//$_SESSION['screen']['width']=$_GET['s'];
+if (!isset($_SESSION['screen']['width'])){$_SESSION['screen']['width']=$_GET['s'];}
+$_SESSION['wid']=$_SESSION['screen']['width'];
+
+
+
+
+
+?>
