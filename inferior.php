a564ff6b620d61a8bccfcbc485f590951be78b93
Frank Barrera <frankb2320@gmail.com>
2016-05-11 17:27:27 -0400

    separando codigo de index.php

---
+++ inferior.php
@@ -0,0 +1,12 @@
+				<section class='inferior'>
+				    <section class='block5'>
+					<section class='tagsec'>
+					    <section class='recnar'></section>
+					    <section class='recneg'></section>
+					    <p class="txteti">Galer&iacute;a de Videos</p>
+					</section>
+					<section class="video_pie widget_pie">
+					    <?php if (!dynamic_sidebar('Youtube'));?>
+					</section>
+				    </section>
+				</section>
