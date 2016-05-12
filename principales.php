920c8efe337d45627e13c2e290728f716cc9a46b
Frank Barrera <frankb2320@gmail.com>
2016-05-12 11:01:47 -0400

    agregando fuentes y ajustando responsive

--- principales.php
+++ principales.php
@@ -1,4 +1,4 @@
-<script async type="text/javascript">jQuery(document).ready(function($){$('#Slider').bjqs({'prevtext':'<span class="icon-circle-left"></span>','nexttext':'<span class="icon-circle-right"></span>','keyboardnav':true,'animtype':'fade','height':515,'width':710,'responsive':true,'showcontrols':true,'showmarkers':false,'randomstart':true,'animspeed':10000});});</script>
+<script async type="text/javascript">jQuery(document).ready(function($){$('#Slider').bjqs({'prevtext':'<span class="icon-chevron-left"></span>','nexttext':'<span class="icon-chevron-right"></span>','keyboardnav':true,'animtype':'fade','height':515,'width':710,'responsive':true,'showcontrols':true,'showmarkers':false,'randomstart':true,'animspeed':10000});});</script>
 				    <section class='principales'>
 					<section class='Slider' id='Slider'>
 						<ul class='bjqs'>
