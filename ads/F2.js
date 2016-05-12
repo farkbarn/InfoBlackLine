95e149541ca2be3ee7f0378dae09c0fa2a013d1e
Frank Barrera <frankb2320@gmail.com>
2016-05-12 12:35:06 -0400

    soporte source en ads

--- ads/F2.js
+++ ads/F2.js
@@ -4,7 +4,7 @@
 											   var m3_r = Math.floor(Math.random()*99999999999);
 											   if (!document.MAX_used) document.MAX_used = ',';
 											   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
-											   document.write ("?zoneid=105");
+											   document.write ("?zoneid=105&amp;source=www.elinformador.com.ve&amp;charset=UTF-8&amp;block=1");
 											   document.write ('&amp;cb=' + m3_r);
 											   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
 											   document.write ('&amp;charset=UTF-8');
