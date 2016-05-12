51fde0d7e2d83b8384bb2de0a56fb8aa45ea8a2a
Frank Barrera <frankb2320@gmail.com>
2016-05-12 10:05:23 -0400

    pruebas con ads

--- ads1.php
+++ ads1.php
@@ -1,10 +1,12 @@
 			<aside class='ads1' id='ads1'>
 			    <ul class='bjqs'>
+				<?php if($_SESSION['wid']>$_SESSION['A1']){?>
 				<li>
 				    <aside class='A1'>
 					<?php include('ads/A1.js');?>
 				    </aside>
 				</li>
+				<?php }?>
 				<li>
 				    <aside class='A2'>
 					<?php include('ads/A2.js');?>
@@ -13,6 +15,7 @@
 					<?php include('ads/A3.js');?>
 				    </aside>
 				</li>
+				<?php if($_SESSION['wid']>$_SESSION['B']){?>
 				<li>
 				    <aside class='B1'>
 					<?php include('ads/B1.js');?>
@@ -21,5 +24,6 @@
 					<?php include('ads/B2.js');?>
 				    </aside>
 				</li>
+				<?php }?>
 			    </ul>
 			</aside>
