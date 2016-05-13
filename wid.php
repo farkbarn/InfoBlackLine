bf6716cc3069563afec012a7009dc497ac705ef3
Frank Barrera <frankb2320@gmail.com>
2016-05-13 12:22:20 -0400

    mejorando los datos a enviar

--- wid.php
+++ wid.php
@@ -3,8 +3,8 @@ session_start();
 if(isset($_POST['width']) && isset($_POST['height'])) {
     $_SESSION['wid']=$_POST['width'];
     $_SESSION['hei']=$_POST['height'];
-    echo json_encode(array('outcome'=>'1'));
+    echo json_encode(array('val'=>true,'wid'=>$_SESSION['wid'],'hei'=>$_SESSION['hei']));
 } else {
-    echo json_encode(array('outcome'=>'0'));
+    echo json_encode(array('val'=>false));
 }
 ?>
