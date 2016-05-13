6f4b4a92db5efbb326d14426530c3c9ba7d8c1c5
farkbarn <frankbarrerag@gmail.com>
2016-05-12 23:28:28 -0400

    incorporando ads async

--- ads/F1.js
+++ ads/F1.js
@@ -1,20 +1,2 @@
-											<script async defer type='text/javascript'><!--//<![CDATA[
-											   document.MAX_ct0 = unescape('INSERT_ENCODED_CLICKURL_HERE');
-											   var m3_u = (location.protocol=='https:'?'https://ads.elinformador.com.ve/www/delivery/ajs.php':'http://ads.elinformador.com.ve/www/delivery/ajs.php');
-											   var m3_r = Math.floor(Math.random()*99999999999);
-											   if (!document.MAX_used) document.MAX_used = ',';
-											   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
-											   document.write ("?zoneid=104&amp;source=www.elinformador.com.ve&amp;charset=UTF-8&amp;block=1");
-											   document.write ('&amp;cb=' + m3_r);
-											   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
-											   document.write ('&amp;charset=UTF-8');
-											   document.write ("&amp;loc=" + escape(window.location));
-											   if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
-											   if (document.context) document.write ("&context=" + escape(document.context));
-											   if ((typeof(document.MAX_ct0) != 'undefined') && (document.MAX_ct0.substring(0,4) == 'http')) {
-												   document.write ("&amp;ct0=" + escape(document.MAX_ct0));
-											   }
-											   if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
-											   document.write ("'><\/scr"+"ipt>");
-											//]]>-->
-											</script>
+<ins data-revive-zoneid="110" data-revive-source="www.elinformador.com.ve" data-revive-ct0="INSERT_ENCODED_CLICKURL_HERE" data-revive-block="1" data-revive-blockcampaign="1" data-revive-id="463250981a193b4550cb94dc91cc70b7"></ins>
+<script async src="//ads.elinformador.com.ve/www/delivery/asyncjs.php"></script>
