8a725fec7dcf9bdaccd0d5985295a14ff859735d
Frank Barrera <frankb2320@gmail.com>
2016-05-13 15:00:46 -0400

    agregando widgets y un general

--- widget.php
+++ widget.php
@@ -1,12 +1,15 @@
 						<section class='widget-home'>
-						    <?php if (is_home()){get_sidebar('Home');}?>
+						    <?php if (is_home()){dynamic_sidebar('Home');}echo 'Home';?>
 						</section>
-						<section class='widget-home'>
-						    <?php if (is_single()){get_sidebar('Nota');}?>
+						<section class='widget-nota'>
+						    <?php if (is_single()){dynamic_sidebar('Nota');echo 'Nota';}?>
 						</section>
-						<section class='widget-home'>
-						    <?php if (is_category()){get_sidebar('Categoria');}?>
+						<section class='widget-categoria'>
+						    <?php if (is_category()){dynamic_sidebar('Categoria');echo 'Categoria';}?>
 						</section>
-						<section class='widget-home'>
-						    <?php if (is_search()){get_sidebar('Busqueda');}?>
+						<section class='widget-busqueda'>
+						    <?php if (is_search()){dynamic_sidebar('Busqueda'); echo 'Busqueda';}?>
+						</section>
+						<section class='widget-general'>
+						    <?php dynamic_sidebar('General');?>
 						</section>
