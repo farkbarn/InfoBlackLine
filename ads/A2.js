4306338d197fb2334cdd026f92289f5e2b724e51
Frank Barrera <frankb2320@gmail.com>
2016-05-13 10:23:26 -0400

    mod en ads

--- ads/A2.js
+++ ads/A2.js
@@ -1,2 +1,3 @@
-<ins data-revive-zoneid="102" data-revive-source="www.elinformador.com.ve" data-revive-ct0="INSERT_ENCODED_CLICKURL_HERE" data-revive-block="1" data-revive-blockcampaign="1" data-revive-id="463250981a193b4550cb94dc91cc70b7"></ins>
-<script async src="//ads.elinformador.com.ve/www/delivery/asyncjs.php"></script>
+<script type='text/javascript'><!--// <![CDATA[
+    OA_show(102);
+// ]]> --></script>
