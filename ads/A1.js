5cf74d4165e887105708417c98665c795e98cf31
Frank Barrera <frankb2320@gmail.com>
2016-05-13 15:54:30 -0400

    update

--- ads/A1.js
+++ ads/A1.js
@@ -1,3 +1,2 @@
-<script type='text/javascript'><!--// <![CDATA[
-    OA_show(101);
-// ]]> --></script>
+<ins data-revive-zoneid="101" data-revive-source="www.elinformador.com.ve" data-revive-ct0="INSERT_ENCODED_CLICKURL_HERE" data-revive-id="463250981a193b4550cb94dc91cc70b7"></ins>
+<script async src="//ads.elinformador.com.ve/www/delivery/asyncjs.php"></script>
