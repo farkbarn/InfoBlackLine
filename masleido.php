8a14d45a98e9addd3f583025dc013c90ec73d097
Frank Barrera <frankb2320@gmail.com>
2016-05-16 13:14:04 -0400

    cambiando etiqueta de block

--- masleido.php
+++ masleido.php
@@ -1,10 +1,5 @@
 						<section class='bmasleido'>
 						    <?php if ($_SESSION['wid']>=$_SESSION['ads8']){include('ads8.php');}?>
 						    <?php if ($_SESSION['wid']>=$_SESSION['ads9']){include('ads9.php');}?>
-						    <section class='tagsec'>
-							<section class='recnar'></section>
-							<section class='recneg'></section>
-							<p class="txteti">Lo M&aacute;s Le&iacute;do</p>
-						    </section>
 						    <?php dynamic_sidebar('Leidos');?>
 						</section>
