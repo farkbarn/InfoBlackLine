e676fda9b35f8ad9df5c16e17e5c61602bc9d408
Frank Barrera <frankb2320@gmail.com>
2016-05-16 15:15:04 -0400

    retirando tags

--- inferior.php
+++ inferior.php
@@ -1,10 +1,5 @@
 				<section class='inferior'>
 				    <section class='block5'>
-					<section class='tagsec'>
-					    <section class='recnar'></section>
-					    <section class='recneg'></section>
-					    <p class="txteti">Galer&iacute;a de Videos</p>
-					</section>
 					<section class="video_pie widget_pie">
 					    <?php dynamic_sidebar('Youtube');?>
 					</section>
