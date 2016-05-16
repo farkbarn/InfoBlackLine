3ff01a8108ccf8bf6d756824372fe26a3ea52552
Frank Barrera <frankb2320@gmail.com>
2016-05-16 14:41:39 -0400

    retirando control

--- widget.php
+++ widget.php
@@ -1,14 +1,14 @@
 						<section class='widget-home'>
-						    <?php if (is_home()){dynamic_sidebar('Home');}echo 'Home';?>
+						    <?php if (is_home()){dynamic_sidebar('Home');}?>
 						</section>
 						<section class='widget-nota'>
-						    <?php if (is_single()){dynamic_sidebar('Nota');echo 'Nota';}?>
+						    <?php if (is_single()){dynamic_sidebar('Nota');}?>
 						</section>
 						<section class='widget-categoria'>
-						    <?php if (is_category()){dynamic_sidebar('Categoria');echo 'Categoria';}?>
+						    <?php if (is_category()){dynamic_sidebar('Categoria');}?>
 						</section>
 						<section class='widget-busqueda'>
-						    <?php if (is_search()){dynamic_sidebar('Busqueda'); echo 'Busqueda';}?>
+						    <?php if (is_search()){dynamic_sidebar('Busqueda');}?>
 						</section>
 						<section class='widget-general'>
 						    <?php dynamic_sidebar('General');?>
