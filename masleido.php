64e151fa485329b70805719e191127c0ee39fa29
Frank Barrera <frankb2320@gmail.com>
2016-05-17 15:24:08 -0400

    add widget

--- masleido.php
+++ masleido.php
@@ -1,5 +1,5 @@
 						<section class='bmasleido'>
 						    <?php if ($_SESSION['wid']>=$_SESSION['ads8']){include('ads8.php');}?>
 						    <?php if ($_SESSION['wid']>=$_SESSION['ads9']){include('ads9.php');}?>
-						    <?php dynamic_sidebar('nota');?>
+						    <?php dynamic_sidebar('Leido');?>
 						</section>
