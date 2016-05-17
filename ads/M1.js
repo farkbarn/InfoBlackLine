7ca8acdc39c40fb5c58d278f1fce070608993623
Frank Barrera <frankb2320@gmail.com>
2016-05-17 16:47:38 -0400

    add ads

---
+++ ads/M1.js
@@ -0,0 +1,2 @@
+<ins data-revive-zoneid="125" data-revive-source="www.elinformador.com.ve" data-revive-ct0="INSERT_ENCODED_CLICKURL_HERE" data-revive-id="463250981a193b4550cb94dc91cc70b7"></ins>
+<script async src="//ads.elinformador.com.ve/www/delivery/asyncjs.php"></script>
