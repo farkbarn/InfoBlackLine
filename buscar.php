b824c5774d2ecf7e34e72a3a118bad6665bcb642
Frank Barrera <frankb2320@gmail.com>
2016-05-17 13:36:33 -0400

    agregando class + id

--- buscar.php
+++ buscar.php
@@ -1,5 +1,5 @@
 		<section class='buscar' id='buscar'>
 		    <form role='search' method='get' action='<?php echo get_site_url();?>'>
-			<input type=search results=5 placeholder=Buscar... name=s>
+			<input class='bus' id='bus' type=search results=5 placeholder=Buscar... name=s>
 		    </form>
 		</section>
