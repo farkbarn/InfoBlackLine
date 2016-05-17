5d41477bc2b7cab2515597b0e647e64050d05f4c
Frank Barrera <frankb2320@gmail.com>
2016-05-17 11:19:53 -0400

    agregando form buscar

---
+++ buscar.php
@@ -0,0 +1,5 @@
+		<section class='buscar' id='buscar'>
+		    <form role='search' method='get' action='<?php echo get_site_url();?>'>
+			<input type=search results=5 placeholder=Buscar... name=s>
+		    </form>
+		</section>
