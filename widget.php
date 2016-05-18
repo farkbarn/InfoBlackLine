379e7d574254c4c20509df7fe032c7afa9caeafc
Frank Barrera <frankb2320@gmail.com>
2016-05-18 12:36:07 -0400

    mejora en pedido

--- widget.php
+++ widget.php
@@ -1,15 +1,23 @@
-						<section class='widget-home'>
-						    <?php if (is_home()){dynamic_sidebar('Home');}?>
-						</section>
+						<?php if (is_home()){?>
+						    <section class='widget-home'>
+							<?php dynamic_sidebar('Home');?>
+						    </section>
+						<?php } ?>
+						<?php if (is_single()){?>
 						<section class='widget-nota'>
-						    <?php if (is_single()){dynamic_sidebar('Nota');}?>
+						    <?php dynamic_sidebar('Nota');?>
 						</section>
+						<?php } ?>
+						<?php if (is_category()){?>
 						<section class='widget-categoria'>
-						    <?php if (is_category()){dynamic_sidebar('Categoria');}?>
+						    <?php dynamic_sidebar('Categoria');?>
 						</section>
+						<?php } ?>
+						<?php if (is_search()){?>
 						<section class='widget-busqueda'>
-						    <?php if (is_search()){dynamic_sidebar('Busqueda');}?>
+						    <?php dynamic_sidebar('Busqueda');?>
 						</section>
+						<?php } ?>
 						<section class='widget-general'>
 						    <?php dynamic_sidebar('Estandar');?>
 						</section>
