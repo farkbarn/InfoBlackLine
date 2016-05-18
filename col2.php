8b4e75c14986cf890aa7bd921078dca6458dd496
Frank Barrera <frankb2320@gmail.com>
2016-05-18 12:39:05 -0400

    control

--- col2.php
+++ col2.php
@@ -56,14 +56,14 @@
 							<?php $_SESSION['i']++; endwhile; ?>
 							<?php wp_reset_query(); ?>
 <!-- FIN NOTA COL2 -->
-						<?php if ($_SESSION['wid']>=500){include('masleido.php');}?>
+						<?php if ($_SESSION['wid']>=$_SESSION['tablet']){include('masleido.php');}?>
 						</section>
 						<section class='block4'>
 						<?php if ($_SESSION['wid']>=$_SESSION['ads10']){include('ads10.php');}?>
 						<?php if ($_SESSION['wid']>=$_SESSION['ads11']){include('ads11.php');}?>
 						<?php if ($_SESSION['wid']>=$_SESSION['ads12']){include('ads12.php');}?>
-						<?php if ($_SESSION['wid']>=500){include('columnistas.php');}?>
-						<?php if ($_SESSION['wid']>=500){include('widget.php');}?>
+						<?php if ($_SESSION['wid']>=$_SESSION['tablet']-200){include('columnistas.php');}?>
+						<?php if ($_SESSION['wid']>=$_SESSION['tablet']-200){include('widget.php');}?>
 						</section>
 						</section>
 						
