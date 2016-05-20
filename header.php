e5ec289be6c9476319bd9f251f85ab96cf1a2881
Frank Barrera <frankb2320@gmail.com>
2016-05-20 13:20:41 -0400

    agregando tagmanager

--- header.php
+++ header.php
@@ -123,6 +123,14 @@ if (isset($userId)) {
   ga('send', 'pageview');
 </script>
 
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MPP3R8"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MPP3R8');</script>
+
 	    <title><?php if (is_home()){echo 'El Informador - Diario Venezolano';} if(is_category()){wp_title(' - El Informador', true, 'right');} if(is_single()){the_title();}?></title>
 	</head>
 	<body>
