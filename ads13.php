a4db9d1a89c2409f22edb4606b7238db13beac29
farkbarn <frankbarrerag@gmail.com>
2016-05-25 15:39:48 -0400

    creando ads13.php

---
+++ ads13.php
@@ -0,0 +1,6 @@
+						    <aside class='ads13' id='ads13'>
+							<ul class='bjqs'>
+							    <li><aside class='N1'><?php include('ads/N1.js');?></aside></li>
+							    <li><aside class='N2'><?php include('ads/N2.js');?></aside></li>
+							</ul>
+						    </aside>
