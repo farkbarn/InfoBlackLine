150575dbf244bdf0b07af9c4f7be7f0bc5d7411d
farkbarn <frankbarrerag@gmail.com>
2016-05-26 12:40:14 -0400

    fecha normalizada

---
+++ fechapie.php
@@ -0,0 +1,3 @@
+<figcaption class='piefotnot'>
+    <section class='txtpie color2'><p class='catnot'><div class="flecha flecol1"></div><span><?php echo list_cat().' | ';?></span><?php if ($_SESSION['wid']>$_SESSION['phone']){the_time('g:ia | d-F |');}else{the_time('ga | F |');}echo ' '.get_the_author();?></p></section>
+</figcaption>
