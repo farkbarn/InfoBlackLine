5a7ddd3ec52ad8ce00399952f225174f9a88a81e
farkbarn <frankbarrerag@gmail.com>
2016-05-26 12:40:27 -0400

    integrando fecha

--- index.php
+++ index.php
@@ -48,9 +48,7 @@
 								?>
 								</a>
 								<?php include('redpie.php');?>
-								<figcaption class='piefotnot'>
-								    <section class='txtpie color2'><p class='catnot'><div class="flecha flecol1"></div><span><?php echo list_cat().' | ';?></span><?php if ($_SESSION['wid']>$_SESSION['phone']){the_time('g:ia | d-F |');}else{the_time('F - ga |');}echo ' '.get_the_author();?></p></section>
-								</figcaption>
+								<?php include('fechapie.php')?>
 							    </figure>
 							</section>
 							<header class='titnot'>
