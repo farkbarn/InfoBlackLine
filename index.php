01a1bc3eb1e9a685c5292431e4462fdc94b4a195
farkbarn <frankbarrerag@gmail.com>
2016-05-26 12:37:31 -0400

    mejora en fecha

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
 								</a>
 								<?php include('redpie.php');?>
 								<figcaption class='piefotnot'>
-								    <section class='txtpie color2'><p class='catnot'><div class="flecha flecol1"></div><span><?php echo list_cat().' | ';?></span><?php if ($_SESSION['wid']>$_SESSION['phone']){the_time('g:i a - d-m-y -');}else{the_time('F - ga |');}echo ' '.get_the_author();?></p></section>
+								    <section class='txtpie color2'><p class='catnot'><div class="flecha flecol1"></div><span><?php echo list_cat().' | ';?></span><?php if ($_SESSION['wid']>$_SESSION['phone']){the_time('g:ia | d-F |');}else{the_time('F - ga |');}echo ' '.get_the_author();?></p></section>
 								</figcaption>
 							    </figure>
 							</section>
