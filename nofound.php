bd47c313bfa478d2eb434fe6460e1853cb639332
farkbarn <frankbarrerag@gmail.com>
2016-05-26 14:49:28 -0400

    control si no se encuentra

--- nofound.php
+++ nofound.php
@@ -1,7 +1,7 @@
 <section class='col1'>
     <section class='nofound' id='nofound'>
 	<p><span> oops! </span></p>
-	<br><br><br><p> Lo sentimos a&uacute;n no tenemos notas que hablen sobre esto <br><br><br> ¿Buscamos otro tema?</p><br><br><br>
+	<br><br><br><p> Lo sentimos a&uacute;n no tenemos notas<br>que hablen sobre esto <br><br><br> ¿Buscamos otro tema?</p><br><br><br>
 	<?php include('buscar.php')?>
     </section>
 </section>
