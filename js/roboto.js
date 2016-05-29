144ea0a4575dfda29a9cb813cc2d6a34aa161009
farkbarn <frankbarrerag@gmail.com>
2016-05-29 17:27:43 -0400

    agregando carga asincrona de fuente

---
+++ js/roboto.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Roboto:400,700,300,400italic,300italic,700italic:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+</script>
