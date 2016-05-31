98dd20f4fba02fe50dabeb331c6e305c8b649886
farkbarn <frankbarrerag@gmail.com>
2016-05-31 14:39:39 -0400

    control de session_start

--- wid.php
+++ wid.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+if(session_status() == PHP_SESSION_NONE){session_start();}
 if(isset($_POST['width']) && isset($_POST['height'])) {
     $_SESSION['wid']=$_POST['width'];
     $_SESSION['hei']=$_POST['height'];
