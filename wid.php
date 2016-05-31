9c2c96c8150cb0920744d3468c7eb9407807cede
farkbarn <frankbarrerag@gmail.com>
2016-05-31 13:45:18 -0400

    mejorando el proceso de captura de pantalla con cookie - pruebas

--- wid.php
+++ wid.php
@@ -3,6 +3,7 @@ session_start();
 if(isset($_POST['width']) && isset($_POST['height'])) {
     $_SESSION['wid']=$_POST['width'];
     $_SESSION['hei']=$_POST['height'];
+    $_SESSION['attwid']=true;
     echo json_encode(array('val'=>true,'wid'=>$_SESSION['wid'],'hei'=>$_SESSION['hei']));
 } else {
     echo json_encode(array('val'=>false));
