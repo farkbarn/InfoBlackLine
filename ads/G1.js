4267e280fecb46cb8723dd53359b4726b415c599
farkbarn <frankbarrerag@gmail.com>
2016-06-01 14:22:13 -0400

    cambios en ads

--- ads/G1.js
+++ ads/G1.js
@@ -1,2 +1,2 @@
-<ins data-revive-zoneid="113" data-revive-source="www.elinformador.com.ve" data-revive-ct0="INSERT_ENCODED_CLICKURL_HERE" data-revive-id="463250981a193b4550cb94dc91cc70b7"></ins>
+<ins data-revive-zoneid="115" data-revive-source="www.elinformador.com.ve" data-revive-ct0="INSERT_ENCODED_CLICKURL_HERE" data-revive-id="463250981a193b4550cb94dc91cc70b7"></ins>
 <script async src="//ads.elinformador.com.ve/www/delivery/asyncjs.php"></script>
--- ads/O1.js
+++
@@ -1,2 +0,0 @@
-<ins data-revive-zoneid="130" data-revive-source="www.elinformador.com.ve" data-revive-ct0="INSERT_ENCODED_CLICKURL_HERE" data-revive-id="463250981a193b4550cb94dc91cc70b7"></ins>
-<script async src="//ads.elinformador.com.ve/www/delivery/asyncjs.php"></script>
