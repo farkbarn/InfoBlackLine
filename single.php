2d03b92de358e32bb3ac26183dad01adfe527f55
farkbarn <frankbarrerag@gmail.com>
2016-06-02 15:26:33 -0400

    mod en single

--- single.php
+++ single.php
@@ -42,7 +42,7 @@
 										);
 								    the_post_thumbnail('col1',$param);
 								}else
-								{echo "<img class='img' src='http://ximg.es/680x340/f29f76ff/fff&text=cargando im&aacute;gen . . .'>";}
+								{echo "<img class='img' src='http://ximg.es/550x274/000000/fff&text=cargando im&aacute;gen . . .'>";}
 								?>
 								</a>
 								<?php include('redpie.php');?>
