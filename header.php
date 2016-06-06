2e0426c688676ab53cad1b69a7886fe28cd3b4f6
farkbarn <frankbarrerag@gmail.com>
2016-06-06 17:48:45 -0400

    agregando meta description

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html lang="es">
 	<head id='head'>
+		<meta name = "description" content =''>
 		<meta name="Author" content="Frank Barrera - @farkbarn">
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
