97783db9a326c26aaf23519edde40503f8d4f0cb
farkbarn <frankbarrerag@gmail.com>
2016-06-10 15:55:23 -0400

    retirando zona M

--- ads14.php
+++ ads14.php
@@ -1,6 +1,6 @@
 						    <aside class='ads14' id='ads14'>
 							<ul class='bjqs'>
 							    <li><aside class='O1'><?php include('ads/M1.js');?></aside></li>
-							    <li><aside class='O2'><?php include('ads/M2.js');?></aside></li>
+							    <li><aside class='O2'><?php //include('ads/M2.js');?></aside></li>
 							</ul>
 						    </aside>
