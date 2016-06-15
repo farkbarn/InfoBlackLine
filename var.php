953b58774cdac927e5107d2061175ceb84bf5777
farkbarn <frankbarrerag@gmail.com>
2016-06-15 15:50:13 -0400

    mejora

--- var.php
+++ var.php
@@ -1,6 +1,5 @@
 <?php
 if(session_status() == PHP_SESSION_NONE){session_start();}
-
 if(isset($_COOKIE['wscr'])){
     if((empty($_SESSION['wid'])) || ($_SESSION['wid']<>$_COOKIE['wscr']) || (isset($_SESSION['wid'])))
     {$_SESSION['wid']=$_COOKIE['wscr'];}
@@ -8,7 +7,6 @@ if(isset($_COOKIE['wscr'])){
 else
 {
     if(empty($_SESSION['wid'])){$_SESSION['wid']=1400;}
-    //setcookie('wscr',$_SESSION['wid'],time()+365*24*60*60,'/');//,'*');
 }
 
 $_SESSION['attwid']=false;
