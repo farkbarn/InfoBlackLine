f6353d532699a32a89d5f37f8a2644c79dc49b51
farkbarn <frankbarrerag@gmail.com>
2016-06-17 15:25:05 -0400

    mejora en carga

--- single.php
+++ single.php
@@ -16,34 +16,36 @@
 							<section>
 							    <figure class='imgnota'>
 								<a href="<?php echo get_permalink();?>">
-								<?php
-								if (has_post_thumbnail()){
-								    $param=array(
-										'class'=>'img imgcol1',
-										'alt'=>get_the_title(),
-										'title'=>get_the_title(),
-										'src'=>wp_get_attachment_image_url(get_post_thumbnail_id(),'col1'),
-										'srcset'=>
-										    wp_get_attachment_image_url(get_post_thumbnail_id(),'col1').' 1x, '.
-										    wp_get_attachment_image_url(get_post_thumbnail_id(),'psli').' 2x, '.
-										    wp_get_attachment_image_url(get_post_thumbnail_id(),'col2').' 3x ',
-										'sizes'=>'
-										    (max-width:1500px) 800px,
-										    (max-width:800px) 700px,
-										    (max-width:600px) 500px,
-										    (max-width:500px) 400px,
-										    (max-width:400px) 300px,
-										    (max-width:300px) 200px,
-										    (max-width:200px) 150px,
-										    (max-width:100px) 100px'
-										);
-								    the_post_thumbnail('col1',$param);
-								}else
-								{echo "<img class='img imgcol1' src='".$_SESSION['dirtem']."img/cargando_550x274.gif'>";}
-								?>
-								</a>
-								<?php include('redpie.php');?>
-								<?php include('fechanota.php');?>
+								<?php if(in_category('carlost') || in_category('hugoc') || in_category('pablos') || in_category('robertom')){ echo "</a>";}
+								else {
+								    if (has_post_thumbnail()){
+									$param=array(
+										    'class'=>'img imgcol1',
+										    'alt'=>get_the_title(),
+										    'title'=>get_the_title(),
+										    'src'=>wp_get_attachment_image_url(get_post_thumbnail_id(),'col1'),
+										    'srcset'=>
+											wp_get_attachment_image_url(get_post_thumbnail_id(),'col1').' 1x, '.
+											wp_get_attachment_image_url(get_post_thumbnail_id(),'psli').' 2x, '.
+											wp_get_attachment_image_url(get_post_thumbnail_id(),'col2').' 3x ',
+										    'sizes'=>'
+											(max-width:1500px) 800px,
+											(max-width:800px) 700px,
+											(max-width:600px) 500px,
+											(max-width:500px) 400px,
+											(max-width:400px) 300px,
+											(max-width:300px) 200px,
+											(max-width:200px) 150px,
+											(max-width:100px) 100px'
+										    );
+									the_post_thumbnail('col1',$param);
+								    }else
+								    {echo "<img class='img imgcol1' src='".$_SESSION['dirtem']."img/cargando_550x274.gif'>";}
+								    ?>
+								    </a>
+								    <?php include('redpie.php');?>
+								    <?php include('fechanota.php');
+								;}?>
 							    </figure>
 							</section>
 							<?php the_content(); endwhile; endif;?>
