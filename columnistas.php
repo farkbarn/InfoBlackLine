7136d3b359fb9c61ab2825acb249ee014d20bbc4
farkbarn <frankbarrerag@gmail.com>
2016-06-17 14:03:56 -0400

    agregando columnistas

--- columnistas.php
+++ columnistas.php
@@ -6,28 +6,44 @@
 						    <p class="txteti">Columnistas</p>
 						</section>
 						<article class='colum'>
+						<?php query_posts(array('posts_per_page'=>1,'category_name' => 'carlost')); ?>
+						<?php while (have_posts()):the_post();?>
 						    <figure>
-							<a href='<?php echo $_SESSION['dircat'];?>carlos_tablante'><img class='img' src='<?php echo $_SESSION['dirtem'];?>img/carlos_tablante.jpg'></a>
+							<!--<a href='<?php echo $_SESSION['dircat'];?>carlos_tablante'>--><img class='img' alt='Carlos Tablante' src='<?php echo $_SESSION['dirtem'];?>img/carlos_tablante.jpg'><!--</a>-->
 							<figcaption>Carlos Tablante</figcaption>
 						    </figure>
+						    <a class='collink' href='<?php echo get_permalink();?>'><h3><?php echo '"'.get_the_title().'"';?></h3></a>
+						<?php endwhile; wp_reset_query();?>
 						</article>
 						<article class='colum'>
+						<?php query_posts(array('posts_per_page'=>1,'category_name' => 'hugoc')); ?>
+						<?php while (have_posts()):the_post();?>
 						    <figure>
-							<a href='<?php echo $_SESSION['dircat'];?>hugo_cabezas'><img class='img' src='<?php echo $_SESSION['dirtem'];?>img/hugo_cabezas.jpg'></a>
+							<!--<a href='<?php echo $_SESSION['dircat'];?>hugo_cabezas'>--><img class='img' alt='Hugo Cabezas' src='<?php echo $_SESSION['dirtem'];?>img/hugo_cabezas.jpg'><!--</a>-->
 							<figcaption>Hugo Cabezas</figcaption>
 						    </figure>
+						    <a class='collink' href='<?php echo get_permalink();?>'><h3><?php echo '"'.get_the_title().'"';?></h3></a>
+						<?php endwhile; wp_reset_query();?>
 						</article>
 						<article class='colum'>
+						<?php query_posts(array('posts_per_page'=>1,'category_name' => 'pablos')); ?>
+						<?php while (have_posts()):the_post();?>
 						    <figure>
-							<a href='<?php echo $_SESSION['dircat'];?>pablo_sepulveda'><img class='img' src='<?php echo $_SESSION['dirtem'];?>img/pablo_sepulveda.jpg'></a>
+							<!--<a href='<?php echo $_SESSION['dircat'];?>pablo_sepulveda'>--><img class='img' alt='Pablo Sepulveda' src='<?php echo $_SESSION['dirtem'];?>img/pablo_sepulveda.jpg'><!--</a>-->
 							<figcaption>Pablo Sep&uacute;lveda</figcaption>
 						    </figure>
+						    <a class='collink' href='<?php echo get_permalink();?>'><h3><?php echo '"'.get_the_title().'"';?></h3></a>
+						<?php endwhile; wp_reset_query();?>
 						</article>
 						<article class='colum'>
+						<?php query_posts(array('posts_per_page'=>1,'category_name' => 'robertom')); ?>
+						<?php while (have_posts()):the_post();?>
 						    <figure>
-							<a href='<?php echo $_SESSION['dircat'];?>roberto_malaver'><img class='img' src='<?php echo $_SESSION['dirtem'];?>img/roberto_malaver.jpg'></a>
+							<!--<a href='<?php echo $_SESSION['dircat'];?>roberto_malaver'>--><img class='img' alt='Roberto Malaver' src='<?php echo $_SESSION['dirtem'];?>img/roberto_malaver.jpg'><!--</a>-->
 							<figcaption>Roberto Malaver</figcaption>
 						    </figure>
+						    <a class='collink' href='<?php echo get_permalink();?>'><h3><?php echo '"'.get_the_title().'"';?></h3></a>
+						<?php endwhile; wp_reset_query();?>
 						</article>
 					    </section>
 					</aside>
