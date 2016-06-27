9e8ff031e46d652fed277048086e58366bc72a66
farkbarn <frankbarrerag@gmail.com>
2016-06-27 13:24:23 -0400

    agregando soporte img-responsive

--- principales.php
+++ principales.php
@@ -32,30 +32,7 @@
 						<section>
 						    <figure class='imgnota'>
 							<a href="<?php echo get_permalink();?>">
-							<?php
-							if (has_post_thumbnail()){
-							    $param=array(
-									'class'=>'img imgpsli',
-									'alt'=>get_the_title(),
-									'title'=>get_the_title(),
-									'srcset'=>
-									    wp_get_attachment_image_url(get_post_thumbnail_id(),'psli').' 1x, '.
-									    wp_get_attachment_image_url(get_post_thumbnail_id(),'psli').' 2x, '.
-									    wp_get_attachment_image_url(get_post_thumbnail_id(),'psli').' 3x ',
-									'sizes'=>'
-									    (max-width:1000px) 800px,
-									    (max-width:800px) 700px,
-									    (max-width:600px) 500px,
-									    (max-width:500px) 400px,
-									    (max-width:400px) 300px,
-									    (max-width:300px) 200px,
-									    (max-width:200px) 150px,
-									    (max-width:100px) 100px'
-									);
-							    the_post_thumbnail('psli',$param);
-							}else
-							{echo "<img class='img imgpsli' src='".$_SESSION['dirtem']."img/cargando_365x183.gif'>";}
-							?>
+							<?php include('img_psli.php'); ?>
 							</a>
 							<?php include('redpie.php');?>
 							<?php include('fechanota.php');?>
