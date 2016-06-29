33eb29ca6ad3cdba3f5317c49a174668885353be
farkbarn <frankbarrerag@gmail.com>
2016-06-29 14:08:48 -0400

    creando link para radio

--- radio.php
+++ radio.php
@@ -1,3 +1,3 @@
-
-
-<iframe src="http://radioshd.com/repro/Estudio967/repro.php" width="600" height="100" frameborder="no" scrolling="No" border="0" framespacing="0"></iframe>
+<section id="radio" class="radio">
+	<iframe src="http://radioshd.com/repro/Estudio967/repro.php" width="300" height="100" frameborder="no" scrolling="No" border="0" framespacing="0"></iframe>
+</section>
