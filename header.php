ded838f0d9b0002663c479affaf17480c3f61c34
farkbarn <frankbarrerag@gmail.com>
2016-07-12 14:23:54 -0400

    intercambio de codigo ga

--- header.php
+++ header.php
@@ -33,6 +33,14 @@
 		<?php }?>
 		<?php if ($_SESSION['wid']<=$_SESSION['tablet']){include('ads/pglevel.js');}?>
 	    <title><?php if (is_home() || is_search() || is_page()){echo 'El Informador - Diario Venezolano';} if(is_category()){wp_title(' - El Informador', true, 'right');} if(is_single()){the_title();}?></title>
+		<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-40942673-1', 'auto');
+		ga('send', 'pageview');
+	    </script>
 	</head>
 	<body>
 		<section class='contenedor' id='contenedor'>
