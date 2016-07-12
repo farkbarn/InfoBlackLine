19b348837a630cd027dc3c88f2a0ec13e107d8ce
farkbarn <frankbarrerag@gmail.com>
2016-07-12 08:48:14 -0400

    ajustando nombre de autor y organizando codigo

--- fechanota.php
+++ fechanota.php
@@ -1,9 +1,4 @@
-<?php if ((is_category())||(is_search())||(is_single())){?>
-<figcaption class='piefotnot'>
-    <section class='txtpie'><p class='catnot'><span><?php echo list_cat().' | ';?></span><?php if ($_SESSION['wid']>$_SESSION['phone']){the_time('g:i-a | d-m-y |');}else{the_time('g:a | d-m-y |');}echo ' '.get_the_author();?></p></section>
-</figcaption>
-<?php }?>
-<?php if (is_home()){?>
+<?php if ((is_category())||(is_search())||(is_home())||(is_single())||(is_archive())){?>
 <figcaption class='piefotnot'>
     <section class='txtpie'><p class='catnot'><span><?php echo list_cat().' | ';?></span><?php if ($_SESSION['wid']>$_SESSION['phone']){the_time('g:ia | d-m-y |');}else{the_time('g:a | d-m-y |');}echo ' '.get_the_author();?></p></section>
 </figcaption>
