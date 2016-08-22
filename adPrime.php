fc4daee88876abd51a721f454bb7e7c619f55dd1
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2016-08-22 14:42:07 -0400

    retirando prime2

--- adPrime.php
+++ adPrime.php
@@ -1,6 +1,5 @@
 					<aside class='adPrime' id='adPrime'>
-					    <ul class='bjqs'>
+					    <ul class='bjqs_'>
 							<li><aside class='Prime1'><?php include('ads/Prime1.js');?></aside></li>
-							<li><aside class='Prime2'><?php include('ads/Prime2.js');?></aside></li>
 					    </ul>
 					</aside>
