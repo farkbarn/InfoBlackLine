5a5b5ee8ce9148077dfb71bc15d0b5806c777341
Frank Barrera (fark) <frankbarrerag@gmail.com>
2017-01-17 10:44:30 -0400

    retirando result=5

--- buscar.php
+++ buscar.php
@@ -1,5 +1,5 @@
 		<section class='buscar' id='buscar'>
 		    <form role='search' method='get' action='<?php echo get_site_url();?>'>
-			<input class='bus' id='bus' type=search results=5 placeholder=Buscar... name=s>
+			<input class='bus' id='bus' type=search placeholder=Buscar... name=s>
 		    </form>
 		</section>
