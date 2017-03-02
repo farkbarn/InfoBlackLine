38dc19f918b45daef93179642a52939c88ad4b7c
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-02 15:38:33 -0400

    reparo

--- functions.php
+++ functions.php
@@ -13,14 +13,14 @@ register_sidebar(array(
     'id'=>'widhome',
     'description'=>'Widget para Home',
     'class'=>'widget',
-    'before_widget'=>'<section class="widget">',
+    'before_widget'=>'<section class="widget"><h3></h3>',
     'after_widget'=>'</section>',
-    'before_title'=>'<section class="tagsec">
-		    <section class="recnar"></section>
-		    <section class="recneg"></section>
-		    <p class="txteti">',
+    'before_title'=>'<section class="tagsec"><h3></h3>
+            <section class="recnar"><h3></h3></section>
+            <section class="recneg"><h3></h3></section>
+            <p class="txteti">',
     'after_title'=>'</p>
-		    </section>'
+            </section>'
 ));
 
 register_sidebar(array(
@@ -28,14 +28,14 @@ register_sidebar(array(
     'id'=>'widest',
     'description'=>'Widget para todos',
     'class'=>'widget',
-    'before_widget'=>'<section class="widget">',
+    'before_widget'=>'<section class="widget"><h3></h3>',
     'after_widget'=>'</section>',
-    'before_title'=>'<section class="tagsec">
-		    <section class="recnar"></section>
-		    <section class="recneg"></section>
-		    <p class="txteti">',
+    'before_title'=>'<section class="tagsec"><h3></h3>
+            <section class="recnar"><h3></h3></section>
+            <section class="recneg"><h3></h3></section>
+            <p class="txteti">',
     'after_title'=>'</p>
-		    </section>'
+            </section>'
 ));
 
 register_sidebar(array(
@@ -43,14 +43,14 @@ register_sidebar(array(
     'id'=>'widlei',
     'description'=>'Widget para Leidos',
     'class'=>'widget',
-    'before_widget'=>'<section class="widget">',
+    'before_widget'=>'<section class="widget"><h3></h3>',
     'after_widget'=>'</section>',
-    'before_title'=>'<section class="tagsec">
-		    <section class="recnar"></section>
-		    <section class="recneg"></section>
-		    <p class="txteti">',
+    'before_title'=>'<section class="tagsec"><h3></h3>
+            <section class="recnar"><h3></h3></section>
+            <section class="recneg"><h3></h3></section>
+            <p class="txteti">',
     'after_title'=>'</p>
-		    </section>'
+            </section>'
 ));
 
 register_sidebar(array(
@@ -58,14 +58,14 @@ register_sidebar(array(
     'id'=>'widyou',
     'description'=>'Canal Youtube El InformadorVE',
     'class'=>'widget',
-    'before_widget'=>'<section class="widget">',
+    'before_widget'=>'<section class="widget"><h3></h3>',
     'after_widget'=>'</section>',
-    'before_title'=>'<section class="tagsec">
-		    <section class="recnar"></section>
-		    <section class="recneg"></section>
-		    <p class="txteti">',
+    'before_title'=>'<section class="tagsec"><h3></h3>
+            <section class="recnar"><h3></h3></section>
+            <section class="recneg"><h3></h3></section>
+            <p class="txteti">',
     'after_title'=>'</p>
-		    </section>'
+            </section>'
 ));
 
 register_sidebar(array(
@@ -73,14 +73,14 @@ register_sidebar(array(
     'id'=>'widcat',
     'description'=>'Widget para Categorias',
     'class'=>'widget',
-    'before_widget'=>'<section class="widget">',
+    'before_widget'=>'<section class="widget"><h3></h3>',
     'after_widget'=>'</section>',
-    'before_title'=>'<section class="tagsec">
-		    <section class="recnar"></section>
-		    <section class="recneg"></section>
-		    <p class="txteti">',
+    'before_title'=>'<section class="tagsec"><h3></h3>
+            <section class="recnar"><h3></h3></section>
+            <section class="recneg"><h3></h3>/section>
+            <p class="txteti">',
     'after_title'=>'</p>
-		    </section>'
+            </section>'
 ));
 
 register_sidebar(array(
@@ -88,14 +88,14 @@ register_sidebar(array(
     'id'=>'widnot',
     'description'=>'Widget para Notas',
     'class'=>'widget',
-    'before_widget'=>'<section class="widget">',
+    'before_widget'=>'<section class="widget"><h3></h3>',
     'after_widget'=>'</section>',
-    'before_title'=>'<section class="tagsec">
-		    <section class="recnar"></section>
-		    <section class="recneg"></section>
-		    <p class="txteti">',
+    'before_title'=>'<section class="tagsec"><h3></h3>
+            <section class="recnar"><h3></h3></section>
+            <section class="recneg"><h3></h3></section>
+            <p class="txteti">',
     'after_title'=>'</p>
-		    </section>'
+            </section>'
 ));
 
 register_sidebar(array(
@@ -103,31 +103,31 @@ register_sidebar(array(
     'id'=>'widbus',
     'description'=>'Widget para Busqueda',
     'class'=>'widget',
-    'before_widget'=>'<section class="widget">',
+    'before_widget'=>'<section class="widget"><h3></h3>',
     'after_widget'=>'</section>',
-    'before_title'=>'<section class="tagsec">
-		    <section class="recnar"></section>
-		    <section class="recneg"></section>
-		    <p class="txteti">',
+    'before_title'=>'<section class="tagsec"><h3></h3>
+            <section class="recnar"><h3></h3></section>
+            <section class="recneg"><h3></h3></section>
+            <p class="txteti">',
     'after_title'=>'</p>
-		    </section>'
+            </section>'
 ));
 
 function the_excerpt_max($charlength) {
     $excerpt=get_the_excerpt();
     $charlength++;
     if ( mb_strlen( $excerpt ) > $charlength ) {
-	$subex = mb_substr( $excerpt, 0, $charlength - 5 );
-	$exwords = explode( ' ', $subex );
-	$excut = - ( mb_strlen( $exwords[ count( $exwords ) - 1 ] ) );
-	if ( $excut < 0 ) {
-	    echo mb_substr( $subex, 0, $excut );
-	} else {
-	    echo $subex;
-	}
-	echo "<a class='lnkexcerpt' href='".get_permalink()."'>...</a>";
+    $subex = mb_substr( $excerpt, 0, $charlength - 5 );
+    $exwords = explode( ' ', $subex );
+    $excut = - ( mb_strlen( $exwords[ count( $exwords ) - 1 ] ) );
+    if ( $excut < 0 ) {
+        echo mb_substr( $subex, 0, $excut );
     } else {
-	echo $excerpt;
+        echo $subex;
+    }
+    echo "<a class='lnkexcerpt' href='".get_permalink()."'>...</a>";
+    } else {
+    echo $excerpt;
     }
 }
 
@@ -137,22 +137,22 @@ $categorias = get_the_category();
 $separador = ' | ';
 $output = '';
     if ( ! empty( $categorias ) ) {
-	foreach( $categorias as $category ) {
-	    if ($category->cat_name=='NOAPLICA' ||
-		$category->cat_name=='DESTACADA' ||
-		$category->cat_name=='SECUNDARIA' ||
-		$category->cat_name=='TEST' ||
-		$category->cat_name=='COLUMNA1A' ||
-		$category->cat_name=='NOUSAR' ||
-		$category->cat_name=='SILUETA' ||
-		$category->cat_name=='Sin categoría' ){
-	    }
-	    else
-	    {
-	    $output .= '<a itemprop="about" href="' . esc_url( get_category_link( $category->term_id ) ) . '" class="categoria '.esc_html( $category->name ).'" title="' . esc_attr( sprintf( __( 'click para %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separador;
-	    }
-	}
-	return trim( $output, $separador );
+    foreach( $categorias as $category ) {
+        if ($category->cat_name=='NOAPLICA' ||
+        $category->cat_name=='DESTACADA' ||
+        $category->cat_name=='SECUNDARIA' ||
+        $category->cat_name=='TEST' ||
+        $category->cat_name=='COLUMNA1A' ||
+        $category->cat_name=='NOUSAR' ||
+        $category->cat_name=='SILUETA' ||
+        $category->cat_name=='Sin categoría' ){
+        }
+        else
+        {
+        $output .= '<a itemprop="about" href="' . esc_url( get_category_link( $category->term_id ) ) . '" class="categoria '.esc_html( $category->name ).'" title="' . esc_attr( sprintf( __( 'click para %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separador;
+        }
+    }
+    return trim( $output, $separador );
     }
 }
 
@@ -196,9 +196,9 @@ add_action( 'admin_head-post-new.php', 'excerpt_count_js');
 function titulo_contador_js(){
       echo '<script>jQuery(document).ready(function(){
 jQuery("#titlewrap").after("<div style=\"position:absolute;top:-32px;right:0px;color:#666;\"><small></small><input type=\"text\" value=\"0\" maxlength=\"3\" size=\"3\" id=\"tit_counter\" readonly=\"\" style=\"background:#fff;\"> <small>Máximo 90 caracter(es).</small></div>");
-	jQuery("#tit_counter").val(jQuery("#title").val().length);
-	jQuery("#title").keyup( function() {
-	jQuery("#tit_counter").val(jQuery("#title").val().length);
+    jQuery("#tit_counter").val(jQuery("#title").val().length);
+    jQuery("#title").keyup( function() {
+    jQuery("#tit_counter").val(jQuery("#title").val().length);
     });
 });</script>';}
 add_action('admin_head-post.php','titulo_contador_js');
@@ -211,6 +211,31 @@ if ($user_login !== "admin") {
 add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
 add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );}
 
+// UPDATE TEMA DESDE WP
+function github_check_update( $transient ) {
+$usergithub=farkbarn;
+$ramastable=master;
+    if ( empty( $transient->checked ) ) {
+        return $transient;
+    }
+    $theme_data = wp_get_theme(wp_get_theme()->template);
+    $theme_slug = $theme_data->get_template();
+    $theme_uri_slug = preg_replace('/-'.$ramastable.'$/', '', $theme_slug);
+   $remote_version = '0.0.0';
+   $style_css = wp_remote_get("https://raw.githubusercontent.com/".$usergithub."/".$theme_uri_slug."/".$ramastable."/style.css")['body'];
+   if ( preg_match( '/^[ \t\/*#@]*' . preg_quote( 'Version', '/' ) . ':(.*)$/mi', $style_css, $match ) && $match[1] )
+       $remote_version = _cleanup_header_comment( $match[1] );
+   if (version_compare($theme_data->version, $remote_version, '<')) {
+       $transient->response[$theme_slug] = array(
+           'theme'       => $theme_slug,
+           'new_version' => $remote_version,
+           'url'         => 'https://github.com/'.$usergithub.'/'.$theme_uri_slug,
+           'package'     => 'https://github.com/'.$usergithub.'/'.$theme_uri_slug.'/archive/'.$ramastable.'.zip',
+       );
+   }
+   return $transient;
+}
+add_filter( 'pre_set_site_transient_update_themes', 'github_check_update' );
 //Cambiar el pie de pagina del panel de Administración
 function change_footer_admin() {echo '&copy;2016 Copyright EL INFORMADOR. Todos los derechos reservados - Web creada por <a href="http://www.elinformador.com.ve">El Informador</a>';}
 add_filter('admin_footer_text', 'change_footer_admin');
