7283d7741151c73eae84bfa66dbc289d890fff97
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-02 15:31:20 -0400

    agregando impreso.php para plantilla

---
+++ impreso.php
@@ -0,0 +1,11 @@
+<?php /* Template Name: impreso */ ?>
+<?php get_header();?>
+    <?php include('var.php');?>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<article class="pag" id="pag">
+				    <header><h2><?php the_title(); ?></h2></header>
+					<?php the_content(); endwhile; endif;?>
+				</article>
+			    </section>
+			</section>
+<?php get_footer(); ?>
