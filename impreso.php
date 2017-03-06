dd28516db82faf3722550d769c881f9dc1b50b15
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-06 13:01:38 -0400

    test

--- impreso.php
+++ impreso.php
@@ -1,29 +1,31 @@
 
-<?php /* Template Name: impreso */ ?>
+<?php /* Template Name: impreso */
+get_header();
 
-<?php get_header();?>
-
-<?php
 $dir='./impreso/';
 $files= scandir($dir);
+include('var.php');
 ?>
-<?php include('var.php');?>
-			<section class="imp_shap">
-			</sectio>
+	<section class="imp_shap"></section>
 <?php
 echo  '<style>
-ul.ulimpr{display:inline-block;text-align:center; margin:0 auto;width:100%;}
-.liimpr{display:inline-block;text-align:center;background-color:#B0B0B0;margin:20px;max-width:250px;}
-.imgimpr{width:100%;height:250px;max-width:200px;margin:10px 0;}
-.impresof{color:white; font-weight:bold;line-height:2em; padding:10px; background-color:#F15A24;margin:20px;
-			display:inline-block;}
+ul.ulimpr{display:inline-block;text-align:center; margin:0 auto;width:100%;text-transform:uppercase;}
+.liimpr{display:inline-block;text-align:center;margin:20px;max-width:250px;border:1px solid #F15A24;}
+.imgimpr{width:100%;height:250px;max-width:200px;margin:10px 0 0 0;}
+.impresof{color:white; font-weight:bold;line-height:2em; padding:10px; background-color:#F15A24;margin: 0;
+			display:inline-block; width:100%;}
 </style>';
 echo '<ul class="ulimpr">';
 	$i=0;
 	foreach ($files as &$val) {
 		if (($val === '.') || ($val === '..') || (true === is_dir($val)) || ('pdf' <> (pathinfo($val)['extension'])) ) {continue;}
 		else {
-echo '<li class="liimpr"><img class="imgimpr" src="../impreso/'.substr($val,0,-4).'.jpg"><a target="_blank" class="impresof" href="../impreso/'.urlencode($val).'" title="'.htmlspecialchars(substr($val,0,-4)).'" >'.htmlspecialchars(substr($val,0,-4)).'</a></li>';
+echo '<li class="liimpr">
+			<a target="_blank" href="../impreso/'.urlencode($val).'" title="'.htmlspecialchars(substr($val,0,-4)).'" >
+				<img class="imgimpr" src="../impreso/'.substr($val,0,-4).'.jpg">
+				<span class="impresof">'.htmlspecialchars(substr($val,0,-4)).'</span>
+			</a>
+		</li>';
 		}
 	}
 echo '</ul>';
